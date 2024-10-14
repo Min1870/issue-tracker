0703f7e08b57e891fd9bbbd83b564f728eab6a3e
minkhantzaw <minkhantzaw.peti@gmail.com>
2024-10-14 11:02:30 +0700

    add assigned to user component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import Navbar from "./Navbar";
 import AuthProvider from "./auth/Provider";
+import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,14 +27,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <AuthProvider>
-          <Theme accentColor="violet">
-            <Navbar />
-            <main className="p-5">
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="violet">
+              <Navbar />
+              <main className="p-5">
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+         </QueryClientProvider>
       </body>
     </html>
   );
