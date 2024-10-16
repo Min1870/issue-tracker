1c992ff1521c4f7d7bd8e7d7c31576bb26f262da
minkhantzaw <minkhantzaw.peti@gmail.com>
2024-10-16 14:50:26 +0700

    fix in migration .sql

--- prisma/migrations/20241016072628_initial_migration/migration.sql
+++ prisma/migrations/20241016072628_initial_migration/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Issue` (
+CREATE TABLE IF NOT EXISTS `Issue` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
